6dff2fa1e62d100ecde57f2dadb1e504f8588efa
Adelssaya <Lorenzonova62@gmail.com>
2025-03-03 11:06:48 -0600

    main.py

---
+++ main.py
@@ -0,0 +1,21 @@
+#Se asigna valor a una variable
+#numero = 10
+
+#Se imprime el valor numero, ej. 10
+#print("El numero asignado es:", numero)
+
+#Actividad de clase: Definir valores y las van a imprimr, sobre algun gobby
+#Usaras:Enteros , Booleanos , Caracteres 
+
+#Asignar variables
+nombre_hobby= "Jugar Videojuegos"
+Tiempo_jugado= " 2 "
+es_divertido= " True "
+nivel_juego= " 100 "
+
+#Imprimir las variables en conjunto a lo que representana
+print("Información sobre el hobby:")
+print("Nombre del hobby:", nombre_hobby)
+print("Tiempo practicando:", Tiempo_jugado, "años")
+print("¿Es divertido?", es_divertido)
+print("Nivel de habilidad:", nivel_juego)
