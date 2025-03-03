74ffaad85999a79592deb6b9d930a0946ba7498e
Adelssaya <Lorenzonova62@gmail.com>
2025-03-03 11:12:33 -0600

    Create Asignacion.py

---
+++ Asignacion.py
@@ -0,0 +1,21 @@
+#Se asigna valor a una variable
+#numero = 10
+
+#Se imprime el valor numero, ej. 10
+#print("El numero asignado es:", numero)
+
+#Actividad de clase: Definir valores y las van a imprimr, sobre algun gobby
+#Usaras:Enteros , Booleanos , Caracteres 
+
+#Asignar variables
+nombre_hobby= "Jugar Videojuegos"
+Tiempo_jugado= " 5 "
+es_divertido= " True "
+tipo_juego= " A "
+
+#Imprimir las variables en conjunto a lo que representana
+print("Información sobre el hobby:")
+print("Nombre del hobby:", nombre_hobby)
+print("Tiempo practicando:", Tiempo_jugado, "años")
+print("¿Es divertido?", es_divertido)
+print("Clasificacion del juego:", tipo_juego)
